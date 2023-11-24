4a13b26946e689054b479628d33fd64f742d0775
MesumAli2 <smesumali20@gmail.com>
2023-11-24 14:03:42 +0400

    h

--- app/src/main/java/com/mesum/cleanarchtodos/TodoRepository.kt
+++ app/src/main/java/com/mesum/cleanarchtodos/TodoRepository.kt
@@ -2,7 +2,7 @@ package com.mesum.cleanarchtodos
 
 import com.mesum.cleanarchtodos.model.Todo
 import javax.inject.Inject
-
+//h
 class TodoRepository @Inject constructor(private val service: JsonPlaceholderService) {
     suspend fun fetchTodos(): List<Todo>{
       return  service.getTodos()
